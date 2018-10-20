c1c66ead62b3a47df544ff153aaca6c7a03cf157
jwwnz <jameswon.nz@gmail.com>
2018-10-20 15:10:58 +1300

    Commit 2: moving buttons

--- buttons/scripts.js
+++ buttons/scripts.js
@@ -7,11 +7,25 @@ function replaceFunction() {
 
 function addFunction() {
   var textValue = document.getElementById("input").value;
+  var selectedColor = document.getElementById("color").value;
   var prevValue = document.getElementById("changethis").innerHTML;
   var textToChange = (document.getElementById("changethis").innerHTML =
-    prevValue + "<div>" + textValue + "</div>");
+    prevValue +
+    '<div style="' +
+    "color: " +
+    selectedColor +
+    '">' +
+    textValue +
+    "</div>");
 }
 
 function deleteFunction() {
   var textToChange = (document.getElementById("changethis").innerHTML = "");
+}
+
+function addColorFunction() {
+  var selectedColor = document.getElementById("color").value;
+  console.log(selectedColor);
+  var textToChange = (document.getElementById("changethis").style =
+    "color: " + selectedColor);
 }
